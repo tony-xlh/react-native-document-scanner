e155dabaf5b3a64c1cc371cb6e1e402f04ed5ec2
xulihang <xulihanghai@163.com>
2022-11-09 14:06:41 +0800

    update MainActivity.java for react-native-screens

--- android/app/src/main/java/com/documentscanner/MainActivity.java
+++ android/app/src/main/java/com/documentscanner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.documentscanner;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -14,7 +15,12 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "DocumentScanner";
   }
-
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+  
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
    * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
